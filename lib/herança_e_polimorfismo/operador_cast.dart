b11b7e57fad5e717a48f1e1ca06bce1d315ee46c
LuanSouzaProg <luandesouza2001@rede.ulbra.br>
2022-02-28 12:02:31 -0300

    DartOrientado_A_Objetos

---
+++ lib/herança_e_polimorfismo/operador_cast.dart
@@ -0,0 +1,75 @@
+void main() {
+  Cachorro cachorro1 = Cachorro("Urso", 3);
+  cachorro1.comer();
+  cachorro1.dormir();
+  cachorro1.latir();
+  print(cachorro1);
+
+  Gato gato1 = Gato("fly", 5);
+  gato1.comer();
+  gato1.dormir();
+  gato1.miar();
+  print(gato1);
+
+  //use cast somente quando tiver certeza do que vai retornar
+  Cachorro animal1 = funcao() as Cachorro;
+  animal1.latir();
+}
+
+Animal funcao(){
+  return Cachorro("Flix", 1);
+}
+
+class Animal{
+  Animal(this.nome, this.idade);
+
+  String nome;
+  int idade;
+
+  void comer(){
+    print("comer");
+  }
+
+  void dormir(){
+    print("dormiu");
+  }
+
+  @override
+  String toString(){
+    return "Nome: $nome, Idade $idade";
+  }
+}
+
+class Cachorro extends Animal{
+  Cachorro(String nome, int idade) : super(nome, idade){
+    print("Criou o cachorro $nome");
+  }
+
+  void latir(){
+    print("Au Au");
+  }
+
+  @override
+  void dormir(){
+    super.dormir();
+    print("Dormiu roncando");
+  }
+
+  @override
+  String toString(){
+    return "Cachorro - Nome: $nome, Idade $idade";
+  }
+}
+
+class Gato extends Animal{
+  Gato(String nome, int idade) : super(nome, idade);
+
+  void miar(){
+    print("Miauuu");
+  }
+
+  @override
+  String toString(){
+    return "Gato - Nome: $nome, Idade $idade";
+  }
+}
